c30cd81ac8f3d5ae8caf7cc2c02db66b8dcdde6c
Doug Turnbull <softwaredoug@gmail.com>
2022-11-03 20:12:48 -0400

    Load old cache if there

--- model.py
+++ model.py
@@ -1,12 +1,41 @@
 from sentence_transformers import SentenceTransformer
 from typing import Iterator, Union
+import pandas as pd
+import numpy as np
+import pickle
+from time import perf_counter
+
+try:
+    print("Loading cache...")
+    start = perf_counter()
+    with open('.cache/all-mpnet-base-v2.pkl', 'rb') as f:
+        cache = pickle.load(f)
+    print(f"Done - {perf_counter() - start}!")
+except IOError:
+    print("No cache available")
+    cache = {}
+
+
+def cache_encode(passages, encoder):
+    if isinstance(passages, str):
+        passages = [passages]
+    to_encode = []
+    encoded = []
+    for passage in passages:
+        try:
+            encoded.append(cache[passage])
+        except KeyError:
+            encoded.append(encoder(passage))
+    return np.array(encoded)
 
 
 class Model:
 
     def __init__(self, model_name):
+        import pdb; pdb.set_trace()
         self.model_name = model_name
         self.model = SentenceTransformer(model_name)
 
     def encode(self, passages: Union[str, Iterator[str]]):
-        return self.model.encode(passages)
+        cache_encoded = cache_encode(passages, self.model.encode)
+        return cache_encoded

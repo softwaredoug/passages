45e9f1e3fa0fe72dbfcd0025d5899605e30c8de3
Doug Turnbull <softwaredoug@gmail.com>
2022-11-25 14:41:58 -0500

    Random projection works in any sign

--- tests/test_random_projection.py
+++ tests/test_random_projection.py
@@ -3,8 +3,26 @@ import numpy as np
 
 # Assuming this math I did a long time ago is right
 # https://github.com/o19s/hangry/blob/master/src/main/java/com/o19s/hangry/randproj/VectorUtils.java#L100
+# You can find a projection (p1...p2) that divides two vectors a and b
+# by solving the inequalities
+#
+#
+# a1 * p1 + a2 * p2 ... + an * pn >= 0
+# b1 * p1 + b2 * b2 ... + bn * pn < 0
+#
+# an * pn >= -(a1 * p1 + a2 * p2 + ... )
+# bn * pn <  -(b1 * p1 + b2 * p2 + ... )
+#
+# an is positive:
+# pn >= -(a1 * p1 + a2 * p2 + ... ) / an
 
-
+# an is negative sign flips:
+# pn < -(a1 * p1 + a2 * p2 + ... ) / an
+#
+# bn has different sign, it will also be
+# pn < -(b1 * p1 + b2 * p2 + ... ) / bn
+#
+#
 def projection_between(vect1: np.ndarray, vect2: np.ndarray):
 
     projection = np.random.random_sample(size=vect1.shape)
@@ -13,15 +31,18 @@ def projection_between(vect1: np.ndarray, vect2: np.ndarray):
     dot1 = np.dot(projection[:-1], vect1[:-1])
     dot2 = np.dot(projection[:-1], vect2[:-1])
 
-    floor = dot1 / vect1[-1]
-    if vect1[-1] > 0:
-        floor = -floor
+    # To get the floor / ceiling we solve an inequality
+    # whose sign flips depending on whether vectn[-1] is
+    # positive or negative
+    if np.sign(vect1[-1]) == np.sign(vect2[-1]):
+        floor = -dot1 / vect1[-1]
+        ceiling = -dot2 / vect2[-1]
+        projection[-1] = (floor + ceiling) / 2.0
+    # Sign is different, then we can just get a large or
+    # small number
+    else:
+        projection[-1] = -10
 
-    ceiling = dot2 / vect2[-1]
-    if vect2[-1] > 0:
-        ceiling = -ceiling
-
-    projection[-1] = (floor + ceiling) / 2.0
     projection /= np.linalg.norm(projection)
     return projection
 
@@ -30,6 +51,36 @@ def test_projection_random_positive():
     for i in range(0, 100):
         vect1 = np.random.random_sample(size=1000)
         vect2 = np.random.random_sample(size=1000)
+        vect1 /= np.linalg.norm(vect1)
+        vect2 /= np.linalg.norm(vect2)
+        projection = projection_between(vect1, vect2)
+        dot1 = np.dot(vect1, projection)
+        dot2 = np.dot(vect2, projection)
+        if dot1 > 0:
+            assert dot2 < 0
+        elif dot1 < 0:
+            assert dot2 > 0
+
+
+def test_projection_random_negative():
+    for i in range(0, 100):
+        vect1 = -np.random.random_sample(size=1000)
+        vect2 = -np.random.random_sample(size=1000)
+        vect1 /= np.linalg.norm(vect1)
+        vect2 /= np.linalg.norm(vect2)
+        projection = projection_between(vect1, vect2)
+        dot1 = np.dot(vect1, projection)
+        dot2 = np.dot(vect2, projection)
+        if dot1 > 0:
+            assert dot2 < 0
+        elif dot1 < 0:
+            assert dot2 > 0
+
+
+def test_projection_random_opposite():
+    for i in range(0, 100):
+        vect1 = np.random.random_sample(size=1000)
+        vect2 = -np.random.random_sample(size=1000)
         vect1 /= np.linalg.norm(vect1)
         vect2 /= np.linalg.norm(vect2)
         projection = projection_between(vect1, vect2)
@@ -57,7 +108,7 @@ def test_projection_between():
         assert dot2 > 0
 
 
-def test_projection_between_opposite_sign():
+def test_projection_between_opposite_sign2():
     vect1 = np.array([0.707, 0.707])
     vect2 = np.array([0.9, -0.1])
     vect1 /= np.linalg.norm(vect1)
